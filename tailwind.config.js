a99e98c5336b30280a4361fa4b20b08ea0c09776
Biel ferrer <bielferrer123@gmail.com>
2025-01-15 01:13:34 +0100

    Añadidos tipos para nodemailer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,9 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
