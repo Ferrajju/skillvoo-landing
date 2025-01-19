a4832a046ec323164b3fd1d00f539d143929f2fe
Biel ferrer <bielferrer123@gmail.com>
2025-01-19 22:18:27 +0100

    Agregando separador desde la seccion About US

--- app/page.tsx
+++ app/page.tsx
@@ -126,7 +126,7 @@ export default function LandingPage() {
   whileInView="animate"
   viewport={{ once: true }}
   variants={staggerChildren}
-  className="container mx-auto py-20 px-4 sm:px-6 lg:px-8 flex flex-col items-center text-center bg-gradient-to-b from-purple-50 to-white rounded-lg shadow-md mb-17"
+  className="container mx-auto py-20 px-4 sm:px-6 lg:px-8 flex flex-col items-center text-center bg-gradient-to-b from-purple-50 to-white rounded-lg shadow-md mb-20"
 >
   {/* Texto principal */}
   <motion.h1 
@@ -159,12 +159,12 @@ export default function LandingPage() {
 
       {/* What is SkillVoo Section */}
       <motion.section 
-        initial="initial"
-        whileInView="animate"
-        viewport={{ once: true }}
-        variants={staggerChildren}
-        className="bg-purple-50 py-20 mt-4"
-      >
+  initial="initial"
+  whileInView="animate"
+  viewport={{ once: true }}
+  variants={staggerChildren}
+  className="bg-purple-50 py-20 mt-20"
+>
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <motion.h2 
             variants={fadeInUp}
