aca432f8685d81901b25c76180b32bc34864efe7
Biel ferrer <bielferrer123@gmail.com>
2025-01-20 16:43:07 +0100

    Agregando traducciones pagina ing por cuarta vez

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,32 +16,32 @@ declare global {
 
 export default function Page() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-    script.async = true;
-    document.body.appendChild(script);
+    // Asegurarte de que el script no se cargue múltiples veces
+    if (!document.querySelector("#google-translate-script")) {
+      const script = document.createElement("script");
+      script.id = "google-translate-script";
+      script.src =
+        "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+      script.async = true;
+      document.body.appendChild(script);
 
-    window.googleTranslateElementInit = () => {
-      const container = document.getElementById("google_translate_element");
-      if (!container) {
-        console.error("El contenedor para Google Translate no existe.");
-        return;
-      }
+      window.googleTranslateElementInit = () => {
+        const container = document.getElementById("google_translate_element");
+        if (!container) {
+          console.error("El contenedor para Google Translate no existe.");
+          return;
+        }
 
-      try {
         new window.google.translate.TranslateElement(
           {
             pageLanguage: "en",
             includedLanguages: "en,es,fr,de,it",
-            layout: 1, // Usar directamente el valor numérico para SIMPLE
+            layout: 1, // 1 para SIMPLE
           },
           "google_translate_element"
         );
-      } catch (error) {
-        console.error("Error al inicializar Google Translate:", error);
-      }
-    };
+      };
+    }
   }, []);
 
   return (
