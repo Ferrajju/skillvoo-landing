95b60d0563c841a8d52fc33194f8fea68ab96ff4
Biel ferrer <bielferrer123@gmail.com>
2025-01-20 14:12:18 +0100

    Ajustando u poco más el btn

--- app/page.tsx
+++ app/page.tsx
@@ -100,14 +100,14 @@ export default function LandingPage() {
         <div className="flex items-center justify-center flex-1">
         <motion.button 
   onClick={(e) => scrollToSection(e, 'registro')}
-  className="bg-gradient-to-r from-purple-600 to-indigo-600 text-white font-semibold py-2 px-6 rounded-full transition-transform duration-300 text-sm inline-flex items-center"
-  style={{ transform: 'translateX(-48%)' }} // Posición fija ligeramente hacia la izquierda
+  className="bg-gradient-to-r from-purple-600 to-indigo-600 text-white font-semibold py-2 px-6 rounded-full transition-transform duration-300 text-sm inline-flex items-center ml-[-48px]"
   whileHover={{ scale: 1.05 }} // Solo efecto de ampliación
   whileTap={{ scale: 0.95 }} // Contracción al hacer clic
 >
   <Mail className="w-4 h-4 mr-2" />
   Únete a la lista de espera
 </motion.button>
+
 
 
     </div>
