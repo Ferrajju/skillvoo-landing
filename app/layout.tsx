2dd34d41b6df121e5aa00a42c4ff461119f58e35
Biel ferrer <bielferrer123@gmail.com>
2025-01-25 21:09:53 +0100

    mejorando dark light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "next-themes"
-import { StyleSheetManager, ThemeProvider as StyledThemeProvider } from "styled-components"
 import ThemeToggle from "../components/ThemeToggle"
+import StyledComponentsRegistry from "../lib/registry"
 import "./globals.css"
 
 export default function RootLayout({
@@ -11,18 +11,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <StyleSheetManager shouldForwardProp={(prop) => prop !== "theme"}>
-            <StyledThemeProvider theme={{}}>
-              <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
-                <nav className="p-4 flex justify-end">
-                  <ThemeToggle />
-                </nav>
-                {children}
-              </div>
-            </StyledThemeProvider>
-          </StyleSheetManager>
-        </ThemeProvider>
+        <StyledComponentsRegistry>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+              <nav className="p-4 flex justify-end">
+                <ThemeToggle />
+              </nav>
+              {children}
+            </div>
+          </ThemeProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   )
