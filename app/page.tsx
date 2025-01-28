fcfea84a73f1d93a80f7da001ea382ee2548a5be
&_Biel_& <Bielferrer123@gmail.com>
2025-01-28 20:09:20 +0100

    Apple

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useState} from "react"
 import { motion, useScroll, useTransform } from "framer-motion"
 import Link from "next/link"
 import { useLanguage } from "@/contexts/language-context"
