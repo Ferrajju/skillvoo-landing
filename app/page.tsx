c40225c57d4e50ee1503e2de98fa796247c58f36
&_Biel_& <Bielferrer123@gmail.com>
2025-02-14 21:40:28 +0100

    Nuevo  btn Powered ai

--- app/page.tsx
+++ app/page.tsx
@@ -200,23 +200,16 @@ export default function LandingPage() {
         </div>
 
         <div className="max-w-7xl mx-auto flex flex-col items-center justify-center relative z-20">
-          <button className="uiverse mb-8">
-            <div className="wrapper">
-              <span>Powered By AI</span>
-              <div className="circle circle-12"></div>
-              <div className="circle circle-11"></div>
-              <div className="circle circle-10"></div>
-              <div className="circle circle-9"></div>
-              <div className="circle circle-8"></div>
-              <div className="circle circle-7"></div>
-              <div className="circle circle-6"></div>
-              <div className="circle circle-5"></div>
-              <div className="circle circle-4"></div>
-              <div className="circle circle-3"></div>
-              <div className="circle circle-2"></div>
-              <div className="circle circle-1"></div>
-            </div>
+          <button className="button">
+          <div className="wrap">
+           <p>
+             <span>✧</span>
+             <span>✦</span>
+                 Pearl Button
+           </p>
+          </div>
           </button>
+
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
